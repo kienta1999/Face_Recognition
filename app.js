65d345e0d024a49ae40d9f1285b17a5641c89148
kienta1999 <talekien1710@gmail.com>
2021-01-24 13:14:33 +0700

    UI done

---
+++ app.js
@@ -0,0 +1,40 @@
+const  express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require("body-parser");
+const {PythonShell} = require('python-shell');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs'); 
+app.use(express.static(path.join(__dirname, 'static')));
+
+let allStudent = [] // { studentID: '123', lastName: 'Kien', firstName: 'Ta'}
+let checkedStudent = [] // 123
+
+app.get('/', function (req, res) {
+   res.render('trang_chu', {name: 'Kien'});
+   
+   PythonShell.run('test.py', {args: [20]}, function (err, res) {
+    if (err) throw err;
+    console.log(res);
+  });
+
+})
+
+app.get('/them_hs', (req, res) => {
+    res.render('them_hs');
+})
+
+app.post('/them_hs', (req, res) => {
+    allStudent.push(req.body);
+    res.redirect('/them_hs');
+})
+
+app.get('/diem_danh', (req, res) => {
+    res.render('diem_danh', {allStudent: allStudent, checkedStudent: checkedStudent});
+})
+
+app.listen(3000, () => {
+    console.log('Application running at port 3000');
+})
---
+++ test.py
@@ -0,0 +1,7 @@
+import sys
+
+if __name__ == "__main__":
+    m = {}
+    m['name'] = 'Kien'
+    m['age'] = int(sys.argv[1])
+    print(m)
